9993bf1728f25016ccc4cf54325faf2c8aa14ed2
danielperezr88 <danielperezr88@gmail.com>
2016-08-26 15:21:45 +0200

    Rearranging grid

--- cancer.py
+++ cancer.py
@@ -61,7 +61,10 @@ def redraw():
 
 
 field_data = req.post(url="http://127.0.0.1:50002/fields", timeout=20).json()
-sliders = WidgetBox(*list(Slider(**dict(zip(list(f.keys())+['callback'],list(f.values())+[callback]))) for f in field_data['results']))
+sliders = WidgetBox(
+    children=list(Slider(**dict(zip(list(f.keys())+['callback'],list(f.values())+[callback]))) for f in field_data['results']),
+    width=30
+)
 
 #scatter = Scatter3d(x='x', y='y', z='z', color='color', data_source=static_source)
 plot = figure(title='PCA Plot', plot_height=300, plot_width=400, responsive=True, tools="pan,reset,save,wheel_zoom")
@@ -69,10 +72,13 @@ plot.scatter(x='x', y='y', color='color', source=static_source)
 
 def_cont = req.post(url="http://127.0.0.1:50002/predict/default", timeout=20).json()
 
-text = Column(Div(text=DIV_TEMPLATE.substitute(**dict(content=def_cont['results'])), sizing_mode='scale_both'))
-spacer = Spacer()
+text = Column(children=[
+    plot,
+    Div(text=DIV_TEMPLATE.substitute(**dict(content=def_cont['results'])), sizing_mode='scale_both')
+], width=65)
+spacer = Spacer(width=5)
 
-curdoc().add_root(gridplot([[text, plot, spacer, sliders]], responsive=True))
+curdoc().add_root(gridplot([[text, spacer, sliders]], responsive=True))
 #curdoc().add_root(gridplot([[script, plot, sliders]], responsive=True))
 
 curdoc().add_next_tick_callback(redraw)
